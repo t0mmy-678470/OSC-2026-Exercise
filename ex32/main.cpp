4d5cfdd32efa37f92d7c3b3c83ef2b0c02cd69b4
t0mmy-678470 <guoyihong64@gmail.com>
2026-04-04 18:16:54 +0800

    finish ex32

--- ex32/main.cpp
+++ ex32/main.cpp
@@ -20,14 +20,102 @@ struct page* get_buddy(struct page* page, unsigned int order) {
     return &mem_map[(page - mem_map.data()) ^ (1 << order)];
 }
 
-void memory_reserve(phys_addr_t base, size_t size) {
-    // TODO: Implement this function
+unsigned int get_pfn(struct page* page) {
+    return (unsigned int)(page - mem_map.data());
 }
 
 void dump() {
     for (int i = MAX_ORDER; i >= 0; i--)
         std::cout << "free_area[" << i << "] " << free_area[i].size()
                   << std::endl;
+}
+
+void memory_reserve(phys_addr_t base, size_t size) {
+    // TODO: Implement this function
+    std::cout << "[Reserve] Reserve address [ "<<base<<", 0x"<<std::hex<<size<<"). Range of pages: ["<<base/PAGE_SIZE<<", "<<(base+size)/PAGE_SIZE<<")" << std::endl;
+
+    unsigned int start_pfn = base/PAGE_SIZE;
+    unsigned int end_pfn = (base+size-1)/PAGE_SIZE; // include
+    int order = MAX_ORDER;
+    while(1) {
+        unsigned int page_order_head_pfn = start_pfn - (start_pfn%(1<<order));
+        int found = 0;
+        // search all free area for this order
+        for(auto cur = free_area[order].begin() ; cur != free_area[order].end() ; cur++) {
+            // found in free list
+            if (*cur == &mem_map[page_order_head_pfn]) {
+                struct page* cur_page = *cur;
+                struct page* buddy_page;
+                found = 1;
+                // full overlap
+                if (page_order_head_pfn == start_pfn && end_pfn >= (page_order_head_pfn+(1<<order)-1)) {
+                    (cur_page)->refcount = 1;
+                    (cur_page)->order = order;
+                    free_area[order].remove(cur_page);
+
+                    start_pfn = page_order_head_pfn+(1<<order);
+                    // check finish
+                    if(start_pfn > end_pfn) {
+                        break;
+                    }
+                    // check still in same MAX_ORDER region
+                    int offset = start_pfn % (1<<MAX_ORDER);
+                    int base = 0;
+                    int next_order=MAX_ORDER-1;
+                    if (offset) {
+                        // compute new order
+                        while(1) {
+                            // find new order
+                            if ( offset == base+(1<<next_order) ) {
+                                break;
+                            }
+                            else if (offset > base+(1<<next_order)) {
+                                base += (1<<next_order);
+                            }
+                            next_order--;
+                            if(next_order<0) {
+                                std::cout<<"ERROR ORDER"<<std::endl;
+                                exit(0);
+                            }
+                        }
+                    }
+                    else {
+                        next_order = MAX_ORDER;
+                    }
+                    // if remain same order, continue the for loop
+                    if(order == next_order) {
+                        continue;
+                    }
+                }
+                // partail overlap
+                // 先 free 掉然後丟到下一個 order 的 free list
+                else if(page_order_head_pfn < start_pfn || end_pfn < (page_order_head_pfn+(1<<order)-1)) {
+                    free_area[order].remove(cur_page);
+                    (cur_page)->order = order - 1;
+                    buddy_page = get_buddy(cur_page, order-1);
+                    buddy_page->order = order - 1;
+                    free_area[order-1].push_front(buddy_page);
+                    free_area[order-1].push_front(cur_page);
+                    order--;
+                }
+
+                break;
+            }
+        }
+        // finished !!!
+        if(start_pfn > end_pfn) {
+            break;
+        }
+        // shouldn't be here
+        if(!found) {
+            std::cout << "\nNot found!" << std::endl;
+            std::cout << "order = " << order << std::endl;
+            std::cout << "start_pfn = 0x" << std::hex << start_pfn << std::endl;
+            std::cout << "end_pfn = 0x" << std::hex << end_pfn << std::endl;
+            std::cout << "page_order_head_pfn = 0x" << std::hex << page_order_head_pfn << std::endl;
+            exit(0);
+        }
+    }
 }
 
 void mm_init() {
@@ -37,6 +125,7 @@ void mm_init() {
         mem_map[i].order = MAX_ORDER;
         free_area[MAX_ORDER].push_back(&mem_map[i]);
     }
+    dump();
     memory_reserve(0, 0x82a69510);
 }
 
--- test.c
+++ test.c
@@ -1,7 +1,49 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(){
+//     char* s = "hello";
+//     printf("%d\n", strlen(s));
+// }
+
+// #include <stdio.h>
+
+// typedef struct list_head {
+//     struct list_head* prev;
+//     struct list_head* next;
+// }LIST_HEAD;
+
+// typedef struct page {
+//     int order;
+//     int refcount;
+//     int refcount2;
+//     char f[3];
+//     struct list_head list;
+// } PAGE;
+
+// #define LIST_ENTRY(ptr, type, memb)  ((type*)((unsigned long)ptr-(unsigned long)&(((type*)0)->memb)))
+
+// int main() {
+//     PAGE p = {1,2,3,'c', 'a', '\0', {(struct list_head*)0x8080, (struct list_head*)0x9090}};
+//     printf("%p: %d %d %d %s %p %p\n", &p, p.order, p.refcount, p.refcount2, p.f, p.list.next, p.list.prev);
+//     printf("%p\n",LIST_ENTRY(&p.list, PAGE, list));
+// }
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(){
-    char* s = "hello";
-    printf("%d\n", strlen(s));
+int main() {
+    char* a1 = (char*) malloc(sizeof(char) * 0x10000);
+    strcpy(a1, "AAAAAAA");
+    char* a2 = (char*) malloc(sizeof(char) * 0x10000);
+    strcpy(a2, "BBBBBBBBBBBBBBB");
+    char* a3 = (char*) malloc(sizeof(char) * 0x10000);
+    strcpy(a3, "CCCCCCCCCCCCCCCCCCCCCCC");
+
+    free(a2);
+    free(a1);
+    free(a3);
+
+    return 0;
 }
